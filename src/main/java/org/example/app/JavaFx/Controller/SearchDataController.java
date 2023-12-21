e320785c0ae40e677da9d61aecd84f64ff999eaf
buitra210 <buitra210ne@gmail.com>
2023-12-21 15:22:47 +0700

    listview

--- src/main/java/org/example/app/JavaFx/Controller/SearchDataController.java
+++ src/main/java/org/example/app/JavaFx/Controller/SearchDataController.java
@@ -2,6 +2,7 @@ package org.example.app.JavaFx.Controller;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,9 +16,14 @@ import java.nio.file.Paths;
 import java.io.IOException;
 import java.io.Reader;
 import java.net.URL;
+import java.util.*;
+import javafx.scene.control.ListView;
+import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-
 import javafx.stage.Stage;
 import org.example.app.JavaFx.objects.hastag;
 public class SearchDataController {
@@ -44,53 +50,32 @@ public class SearchDataController {
         }
         ;
     }
-
+    @FXML
+    private ListView<String> postListView; //thêm khai báo ListView
 
     //@Override
+
     public void initialize(URL location, ResourceBundle resources) {
         try {
             Reader reader = Files.newBufferedReader(Paths.get("/resources/json/Rarible.json"));
-            List<hastag> events = new Gson().fromJson(reader, new TypeToken<List<hastag>>() {
-            }.getType());
-            // festivals.forEach(tmp -> System.out.println(tmp.getFestivalName()));
-            int i = 0;
-            boolean found = false;
-            if (!HelloApplication.findString.equals(""))
-                for (; i < events.size(); i++) {
-                    String name = events.get(i).getUsername().toUpperCase();
-                    if (name.contains(HelloApplication.findString)) {
-                        found = true;
-                        break;
-                    }
-                }
-            if (found) {
-                content.setText(events.get(i).getContent());
-                String[] tmp = events.get(i).getHastags();
-                String s = "";
-                for (String l : tmp) {
-                    s = s + l + "\n";
-                }
-                hastags.setText(s);
-                tmp = new String[]{events.get(i).getDateime()};
-                s = "";
-                for (String l : tmp) {
-                    s = s + l + "\n";
-                }
-                username.setText(events.get(i).getUsername());
-                datetime.setText(s);
-            } else {
-                content.setText("Không rõ");
-                username.setText("Không rõ");
-                datetime.setText("Không rõ");
-                hastags.setText("Không rõ");
+            List<hastag> events = new Gson().fromJson(reader, new TypeToken<List<hastag>>() {}.getType());
+
+            // Tạo danh sách các chuỗi để hiển thị trong ListView
+            ObservableList<String> items = FXCollections.observableArrayList();
+
+            // Lặp qua danh sách events và thêm thông tin từ mỗi sự kiện vào danh sách chuỗi
+            for (hastag event : events) {
+                String info = String.format("Username: %s\nDatetime: %s\nContent: %s\nHashtags: %s\n",
+                        event.getUsername(), event.getDateime(), event.getContent(), Arrays.toString(event.getHastags()));
+                items.add(info);
             }
+
+            // Đặt dữ liệu vào ListView
+            postListView.setItems(items);
+            postListView.setFixedCellSize(100); // Điều chỉnh độ cao tùy thuộc vào nội dung hiển thị
+            postListView.prefHeightProperty().bind(Bindings.size(items).multiply(postListView.getFixedCellSize()).add(2)); // Đặt chiều cao của ListView
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
-
 }
