fd4e48925a03b87edd15f1fc89d7618b21c40061
buitra210 <buitra210ne@gmail.com>
2023-12-23 21:49:45 +0700

    update extends

--- src/main/java/org/example/app/JavaFx/Controller/HotController.java
+++ src/main/java/org/example/app/JavaFx/Controller/HotController.java
@@ -2,13 +2,36 @@ package org.example.app.JavaFx.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class HotController extends HelloController {
@@ -71,18 +94,18 @@ public class HotController extends HelloController {
         ;
     }
 }
-// @FXML
-// protected void Actionback(ActionEvent event) throws IOException {
-// try {
-// Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-// FXMLLoader fxmlLoader = new
-// FXMLLoader(getClass().getResource("/fxml/hello-view.fxml"));
-// Parent root1 = (Parent) fxmlLoader.load();
-// stage.setTitle("Page");
-// stage.setScene(new Scene(root1));
-// stage.show();
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-// ;
-// }
+//    @FXML
+//    protected void Actionback(ActionEvent event) throws IOException {
+//        try {
+//            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/hello-view.fxml"));
+//            Parent root1 = (Parent) fxmlLoader.load();
+//            stage.setTitle("Page");
+//            stage.setScene(new Scene(root1));
+//            stage.show();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        ;
+//    }
+
